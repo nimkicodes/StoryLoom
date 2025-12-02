fccbe2bcfb605cb903ea908cad9344dcbf135671
nimkicodes <deb.mitrasree@gmail.com>
2025-12-02 03:54:45 +0100

    style: Remove serif font from heading and add horizontal rule.

--- src/Create.jsx
+++ src/Create.jsx
@@ -24,10 +24,11 @@ const Create = () => {
     return (
         <div className="flex flex-col text-sl-title min-h-screen pb-10">
             <NavBar />
-            <h1 className="pt-5 pb-2 font-serif font-bold text-sl-title text-4xl text-center">Upload A New Zine</h1>
+            <h1 className="pt-5 pb-2 font-bold text-sl-title text-4xl text-center">Upload A New Zine</h1>
 
             <div className="w-full md:w-7/8 mx-auto px-4 md:px-0">
                 <hr className="border-sl-text"></hr>
+
                 <div className="mt-5 flex flex-col lg:flex-row gap-5 text-sl-text justify-center items-stretch">
                     {/* left box */}
                     <div className="flex flex-col md:flex-row flex-wrap rounded-[1em] w-full lg:w-1/3 p-5 border-solid border-3">
